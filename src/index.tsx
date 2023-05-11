0acacb87aec3232770b0bfbd6ea2d2b0fe46ec3f
Piotr Mikolajczuk <piotr@mikolajczuk.in>
2023-05-11 19:36:47 +0200

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 import { StateMachineProvider, createStore } from "little-state-machine";
 import ChooseDish from "./pages/ChooseDish";
 import Pizza from "./pages/Pizza";
